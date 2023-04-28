8eddc886205eda6cef18cca83633800b47a80e45
Anantakumar Sahu <anantakumar.sahu@qmetry.com>
2023-04-28 08:40:54 +0530

    Initial Commit for Jenkins Hello World

---
+++ HelloWorld_Java_Programme.java
@@ -0,0 +1,6 @@
+public class HelloWorld_Java_Programme {
+    public static void main(String[] args) {
+        System.out.println("********************----Hello, World! and welcome to Jenkins!-----------**************");
+		 System.out.println("********************----This is Anant's Jenkins Profile!-----------**************");
+    }
+}
